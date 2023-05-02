d6efd9fbcdb03e3a5116e9791f335c2b686df0d6
Magomed <dark_punk01@mail.ru>
2023-05-02 18:38:16 +0300

    Fixed requested chenges

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,11 @@ import chat_dots from '../assets/chat_dots.svg';
 
 export const Card = ({ title, image }) => (
   <div>
-    <div>
-      <div className=" p-[.5px] grid grid-cols-2 gap-[75px] bg-[#2C2C2C]">
-        <h5 className="w-[127px] text-white text-left">{title}</h5>
-        <img src={chat_dots} alt="review icon" />
-      </div>
-
-      <img className=" w-[130px]" src={image} alt={title} />
+    <div className=" p-[.5px] grid grid-cols-2 gap-[75px] bg-[#2C2C2C]">
+      <h5 className="w-[127px] text-white text-left">{title}</h5>
+      <img src={chat_dots} alt="review icon" />
     </div>
+
+    <img className=" w-[130px]" src={image} alt={title} />
   </div>
 );
