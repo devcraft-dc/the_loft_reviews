1ec991f71f0dcf94d2f54484cae1965ab5d28668
tepsurkaev <unknownsayid@mail.ru>
2023-05-05 14:59:01 +0300

    Started category filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header';
 import { Search } from './components/Search';
+import { CategoryFilter } from './components/CategoryFilter';
 
 function App() {
   return (
     <>
       <Header />
       <Search />
+      <CategoryFilter />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,18 @@
 const colors = require('tailwindcss/colors');
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
       sm: '350px',
-      md: '400px'
+      md: '400px',
     },
     colors: {
       'main-black': '#2C2C2C',
       'secondary-white': '#E5E5E5',
       gray: colors.gray,
       black: colors.black,
-      white: colors.white
+      white: colors.white,
     },
     extend: {
       backgroundImage: {
@@ -25,5 +22,5 @@ export default {
       }
     },
   },
-  plugins: [],
-}
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
+};
