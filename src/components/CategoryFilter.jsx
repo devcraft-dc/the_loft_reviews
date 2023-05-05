a36e0ff77c2c3d6295ed4f728c0ced912a502db1
tepsurkaev <unknownsayid@mail.ru>
2023-05-05 14:59:01 +0300

    Started category filter

---
+++ src/components/CategoryFilter.jsx
@@ -0,0 +1,34 @@
+import cake from '../assets/cake.svg';
+import drink from '../assets/drink.svg';
+import soup from '../assets/soup.svg';
+import sushi from '../assets/sushi.svg';
+import grill from '../assets/grill.svg';
+import mountain from '../assets/mountain.svg';
+import fastfood from '../assets/fastfood.svg';
+
+const icons = [
+  { icon: cake, title: 'Десерты' },
+  { icon: drink, title: 'Напитки' },
+  { icon: soup, title: 'Супы' },
+  { icon: sushi, title: 'Азия' },
+  { icon: grill, title: 'Мангал' },
+  { icon: mountain, title: 'Нац.кухня' },
+  { icon: fastfood, title: 'Фастфуд' },
+];
+
+export const CategoryFilter = () => (
+  <div className="flex gap-6 p-5 max-md:p-4 max-sm:p-3 items-end bg-main-black overflow-x-auto scrollbar-thin scrollbar-thumb-main-black scrollbar-track-secondary-white scrollbar-thumb-rounded-md">
+    {icons.map((icon) => (
+      <button key={icon.title} type="button">
+        <img
+          src={icon.icon}
+          alt={icon.title}
+          className="w-8 max-md:w-7 max-sm:w-6 mx-auto"
+        />
+        <p className="text-secondary-white text-center text-sm max-md:text-xs max-sm:text-[0.65rem]">
+          {icon.title}
+        </p>
+      </button>
+    ))}
+  </div>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,20 @@
 const colors = require('tailwindcss/colors');
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
       sm: '350px',
-      md: '400px'
+      md: '400px',
     },
     colors: {
       'main-black': '#2C2C2C',
       'secondary-white': '#E5E5E5',
       gray: colors.gray,
       black: colors.black,
-      white: colors.white
+      white: colors.white,
     },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
+};
