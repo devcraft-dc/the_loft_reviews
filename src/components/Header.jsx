cdea47cdddad8ff680e932e16c03b3bd1d2a57b2
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-05-05 16:08:54 +0300

    linked userinfo to data button & set up userinfo adaptive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,57 @@
+import { useState } from 'react';
+import { UserInfo } from './UserInfo';
 import food_icon from '../assets/food.svg';
 import staff_icon from '../assets/staff.svg';
 import userinfo_icon from '../assets/userinfo.svg';
 import white_logo from '../assets/white_logo.png';
 
-export const Header = () => (
-  <header className="bg-main-black flex justify-between items-center h-[5.5rem] max-sm:h-[4.75rem] text-secondary-white px-5">
-    <img
-      className="w-15 h-11 max-md:w-13 max-md:h-8 max-sm:w-13 max-sm:h-7"
-      src={white_logo}
-      alt="logo"
-    />
+export const Header = () => {
+  const [showUserinfo, setShowUserinfo] = useState(false);
 
-    <div className="flex gap-5 max-md:gap-4 p-3 m-3 border-x border-black border-opacity-25">
-      <button>
+  return (
+    <>
+      <header className="bg-main-black flex justify-between items-center h-[5.5rem] max-sm:h-[4.75rem] text-secondary-white px-5">
         <img
-          className="w-8 mx-auto max-md:w-7 max-sm:w-6"
-          src={food_icon}
-          alt="food"
+          className="w-15 h-11 max-md:w-13 max-md:h-8 max-sm:w-13 max-sm:h-7"
+          src={white_logo}
+          alt="logo"
         />
-        <span className="text-sm max-md:text-xs max-sm:text-[0.65rem] ">
-          Еда
-        </span>
-      </button>
-      <button>
-        <img
-          className="w-8 mx-auto max-md:w-7 max-sm:w-6"
-          src={staff_icon}
-          alt="staff"
-        />
-        <span className="text-sm max-md:text-xs max-sm:text-[0.65rem]">
-          Персонал
-        </span>
-      </button>
-    </div>
 
-    <button>
-      <img
-        className="w-8 mx-auto max-md:w-7 max-sm:w-6"
-        src={userinfo_icon}
-        alt="userinfo"
-      />
-      <span className="text-sm max-md:text-xs max-sm:text-[0.65rem]">
-        Данные
-      </span>
-    </button>
-  </header>
-);
+        <div className="flex gap-5 max-md:gap-4 p-3 m-3 border-x border-black border-opacity-25">
+          <button>
+            <img
+              className="w-8 mx-auto max-md:w-7 max-sm:w-6"
+              src={food_icon}
+              alt="food"
+            />
+            <span className="text-sm max-md:text-xs max-sm:text-[0.65rem] ">
+              Еда
+            </span>
+          </button>
+          <button>
+            <img
+              className="w-8 mx-auto max-md:w-7 max-sm:w-6"
+              src={staff_icon}
+              alt="staff"
+            />
+            <span className="text-sm max-md:text-xs max-sm:text-[0.65rem]">
+              Персонал
+            </span>
+          </button>
+        </div>
+
+        <button onClick={() => setShowUserinfo(!showUserinfo)}>
+          <img
+            className="w-8 mx-auto max-md:w-7 max-sm:w-6"
+            src={userinfo_icon}
+            alt="userinfo"
+          />
+          <span className="text-sm max-md:text-xs max-sm:text-[0.65rem]">
+            Данные
+          </span>
+        </button>
+      </header>
+      {showUserinfo && <UserInfo setShowUserinfo={setShowUserinfo} />}
+    </>
+  );
+};
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,40 +1,57 @@
+import { useState } from 'react';
 import close_button from '../assets/close_button.svg';
 
-export const UserInfo = () => (
-  <div className="w-[100%] border-b-[2px] border-black mt-[2px] rounded-b-[20px]">
-    <div className="w-[100%] flex justify-between p-3.5 mb-[10px] bg-main-black text-secondary-white">
-      <h3 className="w-[70%]">Данные рецензента</h3>
-      <button className="">
-        <img src={close_button} alt="" />
-      </button>
-    </div>
-    <form className="text-center flex flex-col gap-3">
-      <div>
-        <input
-          className="text-lg max-sm:text-base  w-[90%] text-secondary-white border-[1px] 
-          border-black bg-main-black placeholder:text-secondary-white rounded-md p-2"
-          type="text"
-          name="name"
-          placeholder="Имя (не обязательно)"
-        />
+export const UserInfo = ({ setShowUserinfo }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  return (
+    <div className="w-full border-b-[2px] border-black mt-[2px] rounded-b-[20px]">
+      <div className="w-full flex justify-between p-3.5 mb-[10px] bg-main-black text-secondary-white">
+        <h3 className="w-[70%] max-sm:text-sm">Данные рецензента</h3>
+        <button onClick={() => setShowUserinfo(false)}>
+          <img
+            className="w-7 max-md:w-6 max-sm:w-5"
+            src={close_button}
+            alt=""
+          />
+        </button>
       </div>
-      <div>
-        <input
-          className="text-lg max-sm:text-base w-[90%] text-secondary-white border-[1px] 
-          border-black bg-main-black placeholder:text-secondary-white  mb-[10px] rounded-md p-2"
-          type=""
-          name="phone"
-          placeholder="Номер телефона (не обязательно)"
-        />
+      <form className="text-center flex flex-col gap-3">
+        <div>
+          <input
+            className="text-md max-md:text-sm max-sm:text-xs w-[90%] text-secondary-white border-[1px] 
+          border-black bg-main-black placeholder:text-secondary-white placeholder:opacity-50 rounded-md p-2"
+            type="text"
+            name="name"
+            placeholder="Имя (не обязательно)"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="text-md max-md:text-sm max-sm:text-xs w-[90%] text-secondary-white border-[1px] 
+          border-black bg-main-black placeholder:text-secondary-white placeholder:opacity-50 mb-[10px] rounded-md p-2"
+            type=""
+            name="phone"
+            placeholder="Номер телефона (не обязательно)"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className="w-[90%] flex justify-end max-md:justify-center gap-2 mb-[10px] m-auto">
+        <button
+          onClick={() => setShowUserinfo(false)}
+          className="text-md max-md:text-sm max-sm:text-xs border bg-main-black text-secondary-white border-black rounded-md py-[5px] px-[10px]"
+        >
+          Отмена
+        </button>
+        <button className="text-md max-md:text-sm max-sm:text-xs border bg-main-black text-secondary-white border-black rounded-md py-[5px] px-[15px]">
+          Отправить
+        </button>
       </div>
-    </form>
-    <div className="w-[90%] flex justify-end max-md:justify-center gap-4 mb-[10px] m-auto">
-      <button className="text-lg max-sm:text-sm border bg-main-black text-secondary-white border-black rounded-md py-[5px] px-[10px]">
-        Отмена
-      </button>
-      <button className="text-lg max-sm:text-sm border bg-main-black text-secondary-white border-black rounded-md py-[5px] px-[15px]">
-        Отправить
-      </button>
     </div>
-  </div>
-);
+  );
+};
