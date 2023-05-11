c7facab259aea70287c4023c99abb97a734f09c6
tepsurkaev <unknownsayid@mail.ru>
2023-05-11 16:11:24 +0300

    Convert border bottom into pseudoelement

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,17 +1,19 @@
-export const IconButton = ({ icon, title, onClick, isActive }) => (
-  <button
-    onClick={onClick}
-    className={isActive ? 'border-b-4' : 'border-b-4 border-main-black'}
-  >
-    <img
-      className="w-8 mx-auto max-md:w-7 max-sm:w-6"
-      src={icon}
-      alt={title ? title : 'icon'}
-    />
-    {title !== '' && (
-      <span className="text-sm max-md:text-xs max-sm:text-[0.65rem]">
-        {title}
-      </span>
-    )}
-  </button>
-);
+import classNames from 'classnames';
+
+export const IconButton = ({ icon, title, isActive, ...props }) => {
+  const styles = classNames('text-sm max-md:text-xs max-sm:text-[0.65rem]', {
+    'after:content-[""] after:block after:absolute after:bottom-[-3px] after:w-full after:h-[3px] after:bg-secondary-white':
+      isActive,
+  });
+
+  return (
+    <button className={classNames({ relative: isActive })} {...props}>
+      <img
+        className="w-8 mx-auto max-md:w-7 max-sm:w-6"
+        src={icon}
+        alt={title ? title : 'icon'}
+      />
+      {title !== '' && <span className={styles}>{title}</span>}
+    </button>
+  );
+};
