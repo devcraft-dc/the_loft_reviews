ac0a312b9242a87716ae6399da5028226f4dd9cf
Magomed <dark_punk01@mail.ru>
2023-05-15 17:01:35 +0300

    conflicts resolved

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,11 @@
-import chat_dots from '../assets/chat_dots.svg';
-
 export const Card = ({ title, image }) => (
   <div className="w-full">
     <div className="flex justify-between items-center px-2 py-1 bg-main-black">
-      <h5 className="text-secondary-white text-md max-md:text-sm max-sm:text-xs">
+      <h5 className="text-secondary-white text-md max-md:text-sm max-sm:text-xs text-ellipsis truncate">
         {title}
       </h5>
-      <img
-        className="w-6 max-md:w-5 max-sm:w-4"
-        src={chat_dots}
-        alt="review icon"
-      />
     </div>
 
-    <img className="w-full" src={image} alt={title} />
+    <img className="w-72 h-40 max-md:h-36 max-md:w-56 max-sm:w-48 max-sm:h-28" src={image} alt={title} />
   </div>
 );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,10 @@
 import { Card } from './Card';
-import img from '../assets/img.png';
-
-const arr = Array.from(Array(24).keys());
+import food from '../assets/food.json';
 
 export const Content = () => (
-  <div className="grid grid-cols-3 gap-[3px] mt-[5.65rem] max-md:mt-[4.9rem] max-sm:mt-[4.25rem] mb-[5.9rem] max-md:my-[4.9rem] max-sm:my-[4.15rem]">
-    {arr.map((item) => (
-      <Card key={item} title="Бюдо" image={img} />
+  <div className="grid grid-cols-3 gap-[3px] mt-[5.8rem] max-md:mt-[5.1rem] max-sm:mt-[4.4rem] mb-[6.5rem] max-md:mb-[6rem] max-md:my-[4.9rem] max-sm:my-[4.15rem]">
+    {food.map((item) => (
+      <Card key={item.title} title={item.title} image={item.image} />
     ))}
   </div>
 );
