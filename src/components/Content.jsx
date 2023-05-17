d7d84e9d279d1bb22703d56f307a4159a08203a2
Magomed <dark_punk01@mail.ru>
2023-05-17 17:53:52 +0300

    Completed search

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react';
 import { Card } from './Card';
+import { Context } from '../context';
 
-export const Content = ({ content }) => (
-  <div className="grid grid-cols-3 gap-[3px] mt-[5.6rem] max-md:mt-[4.9rem] max-sm:mt-[4.1rem] mb-[6.6rem] max-md:mb-[5.8rem] max-md:my-[4.9rem] max-sm:mb-[5.1rem] max-sm:my-[4.15rem]">
-    {content.map((item) => (
-      <Card key={item.title} title={item.title} image={item.image} />
-    ))}
-  </div>
-);
+export const Content = ({ content }) => {
+  const { searchText } = useContext(Context);
+
+  const filteredFood = content.filter((dish) =>
+    dish.title.toUpperCase().includes(searchText.toUpperCase())
+  );
+  return (
+    <div className="grid grid-cols-3 gap-[3px] mt-[5.6rem] max-md:mt-[4.9rem] max-sm:mt-[4.1rem] mb-[6.6rem] max-md:mb-[5.8rem] max-md:my-[4.9rem] max-sm:mb-[5.1rem] max-sm:my-[4.15rem]">
+      {filteredFood.map((item) => (
+        <Card key={item.title} title={item.title} image={item.image} />
+      ))}
+    </div>
+  );
+};
