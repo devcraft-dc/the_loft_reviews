e62858677a1de78178de495b7e6cdf5a58be7367
Magomed <dark_punk01@mail.ru>
2023-05-22 15:37:34 +0300

    comlpeted modal window

--- src/components/WelcomeModal.jsx
+++ src/components/WelcomeModal.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import modal_close from '../assets/icons/modal_close.svg';
 import modal_logo from '../assets/modal_logo.png';
 
-export const WelcomeModal = () => (
-  <div className="w-full h-[100vh] relative">
-    <div className="w-[70%] bg-main-black m-auto rounded-lg absolute top-[25%] left-0 right-0">
+export const WelcomeModal = ({ setModalActive }) => {
+  const [checked, setChecked] = useState(false);
+  const saveToLocalStorage = () => {
+    setModalActive(true);
+    localStorage.setItem('modalChecked', checked);
+  };
+  return (
+    <div className="w-96 max-md:w-80 max-sm:w-72 bg-main-black rounded-lg fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <div className="relative">
-        <button className="absolute top-4 right-[20px] max-md:w-8 max-sm:w-6">
+        <button
+          className="absolute top-4 right-[20px] max-md:w-8 max-sm:w-6"
+          onClick={saveToLocalStorage}
+        >
           <img src={modal_close} alt="close icon" />
         </button>
       </div>
@@ -21,13 +30,17 @@ export const WelcomeModal = () => (
     border-black-300 rounded focus:ring-blue-500 
     dark:focus:ring-blue-600  
     dark:bg-gray-700 dark:border-gray-600"
+            onChange={(e) => setChecked(e.target.checked)}
           />
           Не показывать
         </label>
-        <button className="text-main-black text-sm max-md:px-1 max-sm:px-[2px] max-md:py-[1px] max-sm:py-[.8px] px-2 py-1 rounded bg-secondary-white">
+        <button
+          className="text-main-black text-sm max-md:px-1 max-sm:px-[2px] max-md:py-[1px] max-sm:py-[.8px] px-2 py-1 rounded bg-secondary-white"
+          onClick={saveToLocalStorage}
+        >
           Закрыть
         </button>
       </div>
     </div>
-  </div>
-);
+  );
+};
