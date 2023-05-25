69d5d46b19ce57acd69e23b60bf9ccc19beb82c0
hamza <hamzat20031996@gmail.com>
2023-05-25 14:04:32 +0300

    Devided content component into two different components

---
+++ src/components/MenuContent.jsx
@@ -0,0 +1,43 @@
+import { useContext, useState, useEffect } from 'react';
+import { Card } from './Card';
+import { Context } from '../context';
+import { NothingFound } from './NothingFound';
+import { debounce } from 'lodash';
+import food from '../assets/food.json';
+
+export const MenuContent = () => {
+  const { searchText, activeCategory } = useContext(Context);
+  const [filteredFood, setFilterdFood] = useState([]);
+
+  const debouncedFilter = debounce(() => {
+    const filteredFood = food.filter((dish) => {
+      const dishTitle = dish.title.toUpperCase();
+      const searchQuery = searchText.toUpperCase();
+
+      if (activeCategory) {
+        return (
+          dishTitle.includes(searchQuery) && dish.category === activeCategory
+        );
+      }
+
+      return dishTitle.includes(searchQuery);
+    });
+    setFilterdFood(filteredFood);
+  }, 300);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => debouncedFilter(), 300);
+
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText, activeCategory]);
+
+  return (
+    <div className="grid grid-cols-3 gap-[3px] mt-[5.6rem] max-md:mt-[4.9rem] max-sm:mt-[4.1rem] mb-[6.6rem] max-md:mb-[5.8rem] max-md:my-[4.9rem] max-sm:mb-[5.1rem] max-sm:my-[4.15rem]">
+      {filteredFood.map((item) => (
+        <Card key={item.title} title={item.title} image={item.image} />
+      ))}
+      {filteredFood.length === 0 && <NothingFound />}
+    </div>
+  );
+};
