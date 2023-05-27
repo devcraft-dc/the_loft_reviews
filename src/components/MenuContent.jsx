86555b4dfe59db47decdfa2a0a62a1291aa3adc7
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-05-27 20:20:45 +0300

    moved userinfo to welcome modal

--- src/components/MenuContent.jsx
+++ src/components/MenuContent.jsx
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { Card } from './Card';
 import { Context } from '../context';
 import { NothingFound } from './NothingFound';
-import { WelcomeModal } from './WelcomeModal';
 import food from '../assets/food.json';
 
 export const MenuContent = () => {
@@ -27,7 +26,6 @@ export const MenuContent = () => {
 
   return (
     <div className="grid grid-cols-3 gap-[3px] mt-[5.6rem] max-md:mt-[4.9rem] max-sm:mt-[4.1rem] mb-[6.6rem] max-md:mb-[5.8rem] max-md:my-[4.9rem] max-sm:mb-[5.1rem] max-sm:my-[4.15rem]">
-      <WelcomeModal />
       {filteredFood.map((item) => (
         <Card key={item.title} title={item.title} image={item.image} />
       ))}
--- src/components/WelcomeModal.jsx
+++ src/components/WelcomeModal.jsx
@@ -1,45 +1,90 @@
 import { useState } from 'react';
 import modal_close from '../assets/icons/modal_close.svg';
-import modal_logo from '../assets/modal_logo.png';
+import { formatPhoneNumber } from '../utils/formatPhoneNumber';
 
 export const WelcomeModal = ({ setModalActive }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
   const [checked, setChecked] = useState(false);
+
+  const handlePhoneInput = (value) => {
+    const formattedPhoneNumber = formatPhoneNumber(value);
+    setPhone(formattedPhoneNumber);
+  };
+
+  function handleNameInput(value) {
+    const formattedName = value.replace(/[^a-zA-Z0-9\u0400-\u04FF-"' ]/, '');
+    setName(formattedName);
+  }
+
   const saveToLocalStorage = () => {
-    setModalActive(true);
+    setModalActive(false);
+    localStorage.setItem('name', name);
+    localStorage.setItem('phone', phone);
     localStorage.setItem('modalChecked', checked);
   };
-  return (
-    <div className="w-96 max-md:w-80 max-sm:w-72 bg-main-black rounded-lg fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className="relative">
-        <button
-          className="absolute top-4 right-[20px] max-md:w-8 max-sm:w-6"
-          onClick={saveToLocalStorage}
-        >
-          <img src={modal_close} alt="close icon" />
-        </button>
-      </div>
 
-      <div className="w-3/6 m-auto py-6">
-        <img src={modal_logo} alt="logo" />
-      </div>
-      <div className="flex justify-center items-center mb-8">
-        <label className="relative font-medium max-md:text-sm max-sm:text-xs text-secondary-white dark:text-gray-300 text-md px-6">
-          <input
-            type="checkbox"
-            className="absolute left-0 w-5 h-5 max-md:w-4 max-md:h4 max-sm:w-3 max-sm:h3 text-blue-100 bg-gray-500 
+  return (
+    <div className="fixed grid place-items-center z-50 w-full h-full bg-black bg-opacity-75">
+      <div className="w-96 max-md:w-80 max-sm:w-72 bg-main-black rounded-lg fixed">
+        <div className="relative">
+          <button
+            className="absolute top-3 right-[12px] w-8 max-md:w-6 max-sm:w-6"
+            onClick={saveToLocalStorage}
+          >
+            <img src={modal_close} alt="close icon" />
+          </button>
+        </div>
+
+        <div className="w-full pt-10 pb-5 text-white">
+          <h3 className="text-center mb-5 text-xl max-md:text-sm">
+            Добро пожаловать! <br /> Введите ваши данные
+          </h3>
+          <form className="text-center flex flex-col gap-3">
+            <div>
+              <input
+                className="text-md max-md:text-sm max-sm:text-xs w-10/12 text-black border-[1px] 
+          border-white bg-secondary-white placeholder:text-black placeholder:opacity-75 rounded-sm p-1 pl-2"
+                type="text"
+                name="name"
+                placeholder="Имя (не обязательно)"
+                value={name}
+                onChange={(e) => handleNameInput(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                className="text-md max-md:text-sm max-sm:text-xs w-10/12 text-black border-[1px] 
+              border-white bg-secondary-white placeholder:text-black placeholder:opacity-75 mb-[10px] rounded-sm p-1 pl-2"
+                type=""
+                name="phone"
+                placeholder="Номер телефона (не обязательно)"
+                value={phone}
+                onChange={(e) => handlePhoneInput(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
+
+        <div className="flex justify-center items-center mb-8">
+          <label className="relative flex justify-center items-center font-medium max-md:text-sm max-sm:text-xs text-secondary-white dark:text-gray-300 text-md px-6">
+            <input
+              type="checkbox"
+              className="cursor-pointer absolute left-0 w-5 h-5 max-md:w-4 max-md:h4 max-sm:w-3 max-sm:h3 text-blue-100 bg-gray-500 
     border-black-300 rounded focus:ring-blue-500 
     dark:focus:ring-blue-600  
     dark:bg-gray-700 dark:border-gray-600"
-            onChange={(e) => setChecked(e.target.checked)}
-          />
-          Не показывать
-        </label>
-        <button
-          className="text-main-black text-sm max-md:px-1 max-sm:px-[2px] max-md:py-[1px] max-sm:py-[.8px] px-2 py-1 rounded bg-secondary-white"
-          onClick={saveToLocalStorage}
-        >
-          Закрыть
-        </button>
+              onChange={(e) => setChecked(e.target.checked)}
+            />
+            Запомнить
+          </label>
+          <button
+            className="text-main-black text-sm max-md:px-1 max-sm:px-[2px] max-md:py-[1px] max-sm:py-[.8px] px-2 py-1 cursor-pointer rounded bg-secondary-white"
+            onClick={saveToLocalStorage}
+          >
+            Сохранить
+          </button>
+        </div>
       </div>
     </div>
   );
