8d35e7443720c99fcfe892443981f2abf8057904
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-05-31 17:13:13 +0300

    refactoring some components

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { UserInfo } from '../components/UserInfo';
 import food_icon from '../assets/icons/food.svg';
 import staff_icon from '../assets/icons/staff.svg';
 import white_logo from '../assets/white_logo.png';
@@ -11,35 +9,30 @@ const links = [
   { icon: staff_icon, title: 'Персонал', link: '/staff' },
 ];
 
-export const Header = () => {
-  const [showUserinfo, setShowUserinfo] = useState(false);
+export const Header = () => (
+  <div className="fixed top-0 w-[500px] max-lg:w-full">
+    <header className="bg-main-black flex justify-between items-center border-b-[3px] text-secondary-white py-4 max-md:py-3 max-sm:py-2 px-5">
+      <img
+        className="w-15 h-11 max-md:w-13 max-md:h-8 max-sm:w-13 max-sm:h-7"
+        src={white_logo}
+        alt="logo"
+      />
 
-  return (
-    <div className="fixed top-0 w-[500px] max-lg:w-full">
-      <header className="bg-main-black flex justify-between items-center border-b-[3px] text-secondary-white py-4 max-md:py-3 max-sm:py-2 px-5">
-        <img
-          className="w-15 h-11 max-md:w-13 max-md:h-8 max-sm:w-13 max-sm:h-7"
-          src={white_logo}
-          alt="logo"
-        />
-
-        <div className="flex gap-5 max-md:gap-4 relative">
-          {links.map((link) => (
-            <NavLink
-              className={({ isActive }) =>
-                isActive
-                  ? 'after:content-[""] after:block after:bottom-[-3px] after:w-full after:h-[3px] after:bg-secondary-white'
-                  : undefined
-              }
-              key={link.title}
-              to={link.link}
-            >
-              <IconButton icon={link.icon} title={link.title} />
-            </NavLink>
-          ))}
-        </div>
-      </header>
-      {showUserinfo && <UserInfo setShowUserinfo={setShowUserinfo} />}
-    </div>
-  );
-};
+      <div className="flex gap-5 max-md:gap-4 relative">
+        {links.map((link) => (
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? 'after:content-[""] after:block after:bottom-[-3px] after:w-full after:h-[3px] after:bg-secondary-white'
+                : undefined
+            }
+            key={link.title}
+            to={link.link}
+          >
+            <IconButton icon={link.icon} title={link.title} />
+          </NavLink>
+        ))}
+      </div>
+    </header>
+  </div>
+);
