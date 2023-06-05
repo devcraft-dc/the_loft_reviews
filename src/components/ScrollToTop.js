3300edd3068a441c5fef66bb12e3902b63379a1c
hamza <hamzat20031996@gmail.com>
2023-06-05 15:04:28 +0300

    Use prettier and change scroll to top import

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom"
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -7,6 +7,6 @@ const ScrollToTop = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-}
+};
 
 export default ScrollToTop;
